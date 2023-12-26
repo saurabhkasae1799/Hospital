3d53e8a4448802314c8e5ff0dbdcd19cdda895ec
saurabhkasae1799 <97533055+saurabhkasae1799@users.noreply.github.com>
2023-12-26 11:23:05 +0530

    add file

---
+++ hospital.js
@@ -0,0 +1,51 @@
+ let slide = document.querySelectorAll(".patientReview");
+ let card = document.querySelectorAll(".card");
+ let closeBtn = document.getElementById("closeBtn");
+ let connectBtn = document.getElementById("connectBtn");
+
+
+ let count = 0;
+ slide.forEach(function(slide, index) {
+     slide.style.left = `${index * 100}%`
+ })
+
+ function myFun() {
+     slide.forEach(function(curVal) {
+         curVal.style.transform = `translateX(-${count*100}%)`
+     })
+ }
+ setInterval(function() {
+     count++;
+     if (count == slide.length) {
+         count = 0;
+
+     }
+     myFun()
+ }, 2000)
+ card.forEach(function(cards) {
+     cards.addEventListener("click", function() {
+         console.log(cards);
+         document.querySelector(".detail").style.display = "block"
+         document.querySelector(".content").innerHTML = `
+         <img src=${cards.firstElementChild.src} alt="">
+         <div class="contentText">
+                <h1>Alexa Zoan</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam blanditiis dicta quisquam voluptas! Delectus nesciunt totam numquam error dolorum soluta praesentium. Nostrum.</p>
+          </div>`
+
+         closeBtn.addEventListener("click", function() {
+             document.querySelector(".detail").style.display = "none"
+         })
+     })
+ })
+ connectBtn.addEventListener("click", function() {
+     let email = document.getElementById("email");
+     let pass = document.getElementById("password");
+
+     if (email.value == "" && pass.value == "") {
+         alert("Enter Details")
+     } else {
+         alert("You Logged IN")
+     }
+
+ })
